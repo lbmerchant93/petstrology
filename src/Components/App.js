9a9694c870fb062143960a9fadcf60f00020f804
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-03 21:36:30 -0700

    Change App.js from functional component to class component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,24 @@
-
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <header>This is the header</header>
-     <main>This is the main</main>
-     <footer>This is the footer</footer>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super() 
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <header>Petstrology</header>
+      <main>This is the main</main>
+      <footer>This is the footer</footer>
+      </div>
+    )
+  }
+  
 }
 
 export default App;
