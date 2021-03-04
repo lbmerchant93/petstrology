08e261a10b9eb2bdc0e48758699bca112d810319
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-04 12:25:25 -0700

    Add Yesterday, Today, and Tomorrow buttons

--- src/Components/Horoscope/Horoscope.js
+++ src/Components/Horoscope/Horoscope.js
@@ -5,11 +5,9 @@ import aquarius from'../../images/aquarius.png';
 const Horoscope = () => {
     return (
        <main className="horoscope-main">
-            <div className='title-img'>
-                <h2 className='horoscope-sign-title'>Sign Title</h2>
-                <img className='horoscope-sign-img' src={aquarius} alt='aquarius' />
-            </div>
+            <img className='horoscope-sign-img' src={aquarius} alt='aquarius' />
             <div className='details'>
+                <h2 className='horoscope-sign-title'>Sign Title</h2>
                 <ul>
                     <li className='days-date'>Current Date:</li>
                     <li className='days-color'>Color:</li>
@@ -19,6 +17,9 @@ const Horoscope = () => {
                     <li className='days-lucky-time'>Lucky Time:</li>
                     <li className='days-mood'>Mood:</li>
                 </ul>
+                <button className='yesterday'>Yesterday</button>
+                <button className='today'>Today</button>
+                <button className='tomorrow'>Tomorrow</button>
             </div>
        </main>
     )
