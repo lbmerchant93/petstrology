3e4ce6c4319ea00a42ea23bf545f4c4adb9d80d4
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-03 21:32:58 -0700

    Add Header, Main, Footer. Add background image. Define min height for H M F

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header>This is the header</header>
+     <main>This is the main</main>
+     <footer>This is the footer</footer>
+    </div>
+  );
+}
+
+export default App;
