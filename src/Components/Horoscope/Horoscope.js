c85a9fd2738e58e9872f4f5cafe7fd68672242de
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-04 11:53:59 -0700

    Create Horoscope component, define route to Horoscope url displaying Horoscope component

---
+++ src/Components/Horoscope/Horoscope.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Horoscope.css';
+
+const Horoscope = () => {
+    return (
+       <main>This is the Horoscope Component</main>
+    )
+}
+
+export default Horoscope; 
