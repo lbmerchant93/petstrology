6693e7212a768ef441ed8fca01ceb8b3c64dfb67
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-04 12:15:34 -0700

    Layout initial horoscope information for a day

--- src/Components/Horoscope/Horoscope.js
+++ src/Components/Horoscope/Horoscope.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './Horoscope.css';
+import aquarius from'../../images/aquarius.png';
 
 const Horoscope = () => {
     return (
-       <main>This is the Horoscope Component</main>
+       <main className="horoscope-main">
+            <div className='title-img'>
+                <h2 className='horoscope-sign-title'>Sign Title</h2>
+                <img className='horoscope-sign-img' src={aquarius} alt='aquarius' />
+            </div>
+            <div className='details'>
+                <ul>
+                    <li className='days-date'>Current Date:</li>
+                    <li className='days-color'>Color:</li>
+                    <li className='days-compatibility'>Compatibility:</li>
+                    <li className='days-description'>Description:</li>
+                    <li className='days-lucky-numbers'>Lucky Number:</li>
+                    <li className='days-lucky-time'>Lucky Time:</li>
+                    <li className='days-mood'>Mood:</li>
+                </ul>
+            </div>
+       </main>
     )
 }
 
