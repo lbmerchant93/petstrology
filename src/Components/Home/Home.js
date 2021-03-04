98e4c9bb6a9919efab7b34485d4ad8fa281c6820
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-03 22:42:01 -0700

    Build out articles for each sign, display flex them

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <main>This is the Home Main</main>
+        <main className='all-star-signs-main'>
+            <article className='all-star-signs'>Capricorn</article>
+            <article className='all-star-signs'>Aquarius</article>
+            <article className='all-star-signs'>Pisces</article>
+            <article className='all-star-signs'>Aries</article>
+            <article className='all-star-signs'>Taurus</article>
+            <article className='all-star-signs'>Gemini</article>
+            <article className='all-star-signs'>Cancer</article>
+            <article className='all-star-signs'>Leo</article>
+            <article className='all-star-signs'>Virgo</article>
+            <article className='all-star-signs'>Libra</article>
+            <article className='all-star-signs'>Scorpio</article>
+            <article className='all-star-signs'>Sagittarius</article>
+        </main>
     )
 }
 
