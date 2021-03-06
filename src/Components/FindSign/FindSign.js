63e18771fc0a7687ddc3dbb23550d8e6c8fc6539
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-06 13:52:29 -0700

    Change FindSign back to functional component

--- src/Components/FindSign/FindSign.js
+++ src/Components/FindSign/FindSign.js
@@ -1,35 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Form from '../Form/Form'
 import './FindSign.css';
 
-class FindSign extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
+const FindSign = (props) => {
+    let displaySavedDates = props.savedDates.map(saved => {
+        return <h3 key={saved.id}>{saved.name} {saved.date}</h3>
+    })
 
-        }
-    }
-
-    render() {
-        let displaySavedDates = this.props.savedDates.map(saved => {
-           return <h3 key={saved.id}>{saved.name} {saved.date}</h3>
-        })
-
-        return (
-            <main>
-                <h2>Not sure of you or your pets sign? Fill out the form below to find out!</h2>
-                <Form addBirthDate={this.props.addBirthDate} />
-                {!this.props.savedDates.length && 
-                <div>
-                    <h3>No dates saved yet! Fill out the form above to save your loved ones dates! </h3>
-                </div>}
-                {this.props.savedDates.length !== 0 && 
-                <div>
-                    {displaySavedDates}
-                </div>}
-            </main>
-        )
-    } 
+    return (
+        <main>
+            <h2>Find Your Sign</h2>
+            <Form addBirthDate={props.addBirthDate} />
+            {!props.savedDates.length && 
+            <div>
+                <h3>No dates saved yet! Fill out the form above to save your loved ones dates! </h3>
+            </div>}
+            {props.savedDates.length !== 0 && 
+            <div>
+                {displaySavedDates}
+            </div>}
+        </main>
+    )
 }
 
 export default FindSign;
