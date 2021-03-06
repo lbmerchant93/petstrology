f021935f6cfdd9245937106b906f6180cf9b8e32
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-06 12:43:58 -0700

    Create Form component, import to FindSign

--- src/Components/FindSign/FindSign.js
+++ src/Components/FindSign/FindSign.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Form from '../Form/Form'
 import './FindSign.css';
 
 const FindSign = () => {
 
     return (
-        <main>This is the Find Sign page</main>
+        <main>
+            <Form />
+        </main>
     )
 }
 
---
+++ src/Components/Form/Form.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <form>
+                <p>this is the form component</p>
+            </form>
+        )
+    }
+}
+
+export default Form;
