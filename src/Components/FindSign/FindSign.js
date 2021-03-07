c11c47dc012d7af8525840f9d307a127a18e625e
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-06 17:57:29 -0700

    Install Moment.js to use for formating date

--- src/Components/FindSign/FindSign.js
+++ src/Components/FindSign/FindSign.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Form from '../Form/Form'
 import './FindSign.css';
+import moment from 'moment';
 
 const FindSign = (props) => {
     let displaySavedDates = props.savedDates.map(saved => {
-        return <h3 key={saved.id}>{saved.name} {saved.date} {saved.sign}</h3>
+        let formatDate = moment(saved.date).format("MMMM Do")
+        return (
+            <section key={saved.id}>
+                <h3 className='name'>
+                    {saved.name} {formatDate} {saved.sign}
+                </h3>
+            </section>
+        )
     })
     
     return (
@@ -12,13 +20,13 @@ const FindSign = (props) => {
             <h2>Find Your Sign</h2>
             <Form addBirthDate={props.addBirthDate} />
             {!props.savedDates.length && 
-            <div>
+            <div className='none-saved'>
                 <h3>No dates saved yet! Fill out the form above to save your loved ones dates! </h3>
             </div>}
             {props.savedDates.length !== 0 && 
-            <div>
+            <article className='saved-signs'>
                 {displaySavedDates}
-            </div>}
+            </article>}
         </main>
     )
 }
