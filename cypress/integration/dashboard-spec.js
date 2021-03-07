067f34eaefe77b5083097c1a5d28fd099c1e08a7
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-07 14:41:35 -0700

    Test for Find Your Sign link, test the url changes

--- cypress/integration/dashboard-spec.js
+++ cypress/integration/dashboard-spec.js
@@ -7,4 +7,9 @@ describe('Dashboard UI', () => {
         cy.get('header').contains('Petstrology').should('be.visible')
     })
 
+    it('Should have a link to the Find Your Sign page in the header', () => {
+        cy.get('header .saved-signs-link')
+            .click()
+        cy.url().should('include', 'FindYourSigns')
+    })
 })
