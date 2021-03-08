6e68bca9be4840534b285357f185a433679bd831
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-07 18:27:34 -0700

    Test that submitted inputs populate on the page with correct information form inputs

--- cypress/integration/find-sign-spec.js
+++ cypress/integration/find-sign-spec.js
@@ -11,14 +11,16 @@ describe('Dashboard UI', () => {
             .click()
     })
 
-    it('Should have an input you can enter a name and date', () => {
+    it('Should have section to Find Your Sign containing inputs you can enter a name and date', () => {
+        cy.get('.find-title').contains('FIND YOUR SIGN')
         cy.get('.name-input').type('Lucas')
         cy.get('.name-input').should('have.attr', 'value', 'Lucas')
         cy.get('.date-input').type('2021-01-01')
         cy.get('.date-input').should('have.attr', 'value', '2021-01-01')
     })
 
-    it('Should have a place holder text for when there are no searches yet and the searched signs shouldn\'t be visible', () => {
+    it('Should have a section for Your Searched Signs and a placeholder text for when there are no searches yet and the searched signs shouldn\'t be visible', () => {
+        cy.get('.saved-search-title').contains('YOUR SEARCHED SIGNS')
         cy.get('.none-saved').contains('No dates saved yet! Fill out the form above to save your loved one\'s dates!')
         cy.get('.saved-signs').should('not.exist')
     })
@@ -31,6 +33,16 @@ describe('Dashboard UI', () => {
         cy.get('.date-input').type('2021-01-01')
         cy.get('.date-input').should('have.attr', 'value', '2021-01-01')
         cy.get('.submit').should('not.have.attr', 'disabled')
+    })
+
+    it('Should display your inputs when submitted in place of the placeholder message', () => {
+        cy.get('.name-input').type('Lucas')
+        cy.get('.date-input').type('2021-01-01')
+        cy.get('.submit').click()
+        cy.get('.none-saved').should('not.exist')
+        cy.get('.saved-instance .name').contains('Lucas')
+        cy.get('.saved-instance .sign').contains('Capricorn')
+        cy.get('.saved-instance .birth-date').contains('January 1st')
     })
 
 })
